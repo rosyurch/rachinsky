a2df8e8032bfc08a96fa429e78686fd16018fd82
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-12-27 23:11:38 +0200

    score counting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import './css/App.css';
 import generateId from './helpers/id'; // only for a single game
 import AddButton from './AddButton';
+import Player from './Player';
+import './css/App.css';
+
+const mockedPlayersState = [
+    { id: 0, name: '123', answers: [10, -20, 30, -10, 20] },
+    { id: 1, name: '456', answers: [] },
+    { id: 9, name: '89+', answers: [] },
+];
 
 function App() {
-    const [players, setPlayers] = useState([
-        // mocked players
-        { id: 0, name: '123', answers: [10, -20, 30, -10, 20] },
-        { id: 1, name: '456', answers: [] },
-        { id: 9, name: '89+', answers: [] },
-    ]);
+    const [players, setPlayers] = useState(mockedPlayersState);
     const [addPopupIsOpened, setAddPopupIsOpened] = useState(false);
     const [name, setName] = useState(''); // for getting player name form popup
 
@@ -33,16 +35,27 @@ function App() {
         setAddPopupIsOpened(false);
     };
 
+    const registerAnswer = (id, name, answers, price) => {
+        const playersWithoutCurrent = players.filter(p => p.id !== id);
+
+        const registeredAnswer = [...answers, price];
+        const newState = [...playersWithoutCurrent, { id, name, answers: registeredAnswer }];
+
+        setPlayers(newState);
+    };
+
     return (
         <div className="App">
             <header className="header">Свояк9000</header>
 
             <ul className="score-field">
-                {players.map(player => (
-                    <li key={player.id}>
-                        <h3>{player.name}</h3>
-                    </li>
-                ))}
+                {players
+                    .sort((a, b) => a.id - b.id)
+                    .map(player => (
+                        <li key={player.id}>
+                            <Player {...player} press={registerAnswer} /> {/* like an actual button press*/}
+                        </li>
+                    ))}
             </ul>
 
             <AddButton
---
+++ src/Player.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './css/player.css';
+
+function Player({ id, name, answers, press }) {
+    const pressed = price => press(id, name, answers, price);
+    return (
+        <div className="player_card">
+            <h3 className="player_name">{name}</h3>
+            <p className="player_score">{answers.reduce((acc, cur) => acc + cur, 0)}</p>
+            <ul className="player_answers-list">
+                {answers.map(ans => (
+                    <li key={Math.random() * 1000} className={`player_answer player_answer-${ans > 0 ? 'correct' : 'wrong'}`}>
+                        {ans}
+                    </li> // probably not a good solution for the key (with repetitive data)
+                ))}
+            </ul>
+            <hr className="player_answer-button-separator" />
+            <div className="player_buttons player_positive-buttons">
+                <button className="player_button player_button-pos" onClick={e => pressed(10)}>
+                    +10
+                </button>
+                <button className="player_button player_button-pos" onClick={e => pressed(20)}>
+                    +20
+                </button>
+                <button className="player_button player_button-pos" onClick={e => pressed(30)}>
+                    +30
+                </button>
+                <button className="player_button player_button-pos" onClick={e => pressed(40)}>
+                    +40
+                </button>
+                <button className="player_button player_button-pos" onClick={e => pressed(50)}>
+                    +50
+                </button>
+            </div>
+            <div className="player_negative-buttons">
+                <button className="player_button player_button-neg" onClick={e => pressed(-10)}>
+                    -10
+                </button>
+                <button className="player_button player_button-neg" onClick={e => pressed(-20)}>
+                    -20
+                </button>
+                <button className="player_button player_button-neg" onClick={e => pressed(-30)}>
+                    -30
+                </button>
+                <button className="player_button player_button-neg" onClick={e => pressed(-40)}>
+                    -40
+                </button>
+                <button className="player_button player_button-neg" onClick={e => pressed(-50)}>
+                    -50
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Player;
