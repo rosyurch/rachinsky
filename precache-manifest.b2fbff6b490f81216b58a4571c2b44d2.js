487b9bafdeafc54c42df75d3bf811e6adaa041b6
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-12-28 13:40:59 +0200

    Updates

---
+++ precache-manifest.b2fbff6b490f81216b58a4571c2b44d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9202fc3cceed18141c16bafa7affe12b",
+    "url": "/rachinsky/index.html"
+  },
+  {
+    "revision": "b3023cd3a2fa1390576e",
+    "url": "/rachinsky/static/css/main.047851ff.chunk.css"
+  },
+  {
+    "revision": "70772081d6dd2394447b",
+    "url": "/rachinsky/static/js/2.b88a0fef.chunk.js"
+  },
+  {
+    "revision": "b3023cd3a2fa1390576e",
+    "url": "/rachinsky/static/js/main.d58ddcc2.chunk.js"
+  },
+  {
+    "revision": "74c0fc883e6b4ca8199b",
+    "url": "/rachinsky/static/js/runtime-main.d90b4f35.js"
+  }
+]);
